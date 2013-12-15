fa55f62bdc08749169d45ef262a1162fb549f0b5
Matt Boldt <me@mattboldt.com>
2013-12-14 22:38:13 -0600

    moved asset code to proper file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Push::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.assets.initialize_on_precompile = false
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
