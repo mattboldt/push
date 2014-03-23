c77b484a07fdd02f99867960da41e9f1b0c1a702
Matt Boldt <me@mattboldt.com>
2014-03-22 22:22:49 -0500

    added repo access back

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Push::Application.config.client_id, Push::Application.config.client_secret, :scope => 'public_repo,gist,user'
+  provider :github, Push::Application.config.client_id, Push::Application.config.client_secret, :scope => 'repo,gist,user'
 end
