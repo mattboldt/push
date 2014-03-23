5e12aa5a4efd73d4703cfd45574b4a9080f26182
Matt Boldt <me@mattboldt.com>
2014-03-22 21:59:58 -0500

    added user email and username permissions to github api access

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Push::Application.config.client_id, Push::Application.config.client_secret, :scope => 'repo,gist'
+  provider :github, Push::Application.config.client_id, Push::Application.config.client_secret, :scope => 'repo,gist,user,user:email,user:username'
 end
